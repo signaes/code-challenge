972913c14b46792b8064ff48f6621efc09bafc48
Thiago Oliveira <thiago@signaes.co>
2017-10-26 09:58:20 -0200

    refactor: Update initial state

--- batmap/src/index.js
+++ batmap/src/index.js
@@ -10,25 +10,34 @@ import load from './utils/load';
 import { GOOGLE_MAPS_API_KEY } from './env';
 import batman from './resources/batman';
 
+const initialState = maps => ({
+  map: {
+    mapsApi: maps,
+    mapObject: {},
+    markers: {
+      list: []
+    }
+  },
+  batman: {
+    currentPosition: batman.getCurrentPosition()
+  },
+  villain: {
+    name: '',
+    location: {},
+    targets: {
+      active: {},
+      list: []
+    }
+  }
+});
+
 load(`https://maps.googleapis.com/maps/api/js?key=${GOOGLE_MAPS_API_KEY}`)
   .then(({ maps }) => {
     if (typeof maps === 'undefined') {
       throw new Error('maps is undefined');
     }
 
-    const store = initializeStore({
-      map: {
-        mapsApi: maps,
-        mapObject: {},
-        markers: {
-          list: []
-        }
-      },
-      batman: {
-        currentPosition: batman.getCurrentPosition()
-      },
-
-    });
+    const store = initializeStore(initialState(maps));
 
     ReactDOM.render(
       <Provider store={store}>
