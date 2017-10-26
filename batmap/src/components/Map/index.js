496e9a1797847e4fe9dbb05d327ec5bb3907221b
Thiago Oliveira <thiago@signaes.co>
2017-10-26 00:38:29 -0200

    feat: Add Map component to render google maps

---
+++ batmap/src/components/Map/index.js
@@ -0,0 +1,39 @@
+/*eslint no-unused-vars: ["error", { "varsIgnorePattern": "React" }]*/
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import './styles.css';
+
+class Map extends Component {
+  componentDidMount() {
+    const { initializeMap, addMarker, findVillain } = this.props;
+    const { currentPosition } = this.props.batman;
+    const { lat, lng } = currentPosition;
+    console.log('Batman you’re at', currentPosition);
+
+    initializeMap({
+      container: this.container,
+      center: currentPosition
+    });
+    addMarker({
+      lat,
+      lng,
+      title: 'Batman'
+    });
+    findVillain(currentPosition);
+  }
+
+  render() {
+    return (
+      <div className="Map" ref={ div => this.container = div }>
+      </div>
+    );
+  }
+}
+
+Map.propTypes = {
+  mapsApi: PropTypes.object,
+  batman: PropTypes.object,
+  initializeMap: PropTypes.func
+};
+
+export default Map;
