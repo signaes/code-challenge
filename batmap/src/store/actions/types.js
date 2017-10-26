3230ee004158cd97f6122aa36bab145b998dabed
Thiago Oliveira <thiago@signaes.co>
2017-10-26 10:00:10 -0200

    feat: Add new action types, ADD_VILLAIN, ADD_TARGETS, SELECT_TARGET

--- batmap/src/store/actions/types.js
+++ batmap/src/store/actions/types.js
@@ -3,4 +3,7 @@ export const NOT_FETCHING = 'notFetching';
 export const NOT_FOUND = 'notFound';
 export const INITIALIZE_MAP = 'initializeMap';
 export const ADD_MARKER = 'addMarker';
+export const ADD_VILLAIN = 'addVillain';
+export const ADD_TARGETS = 'addTargets';
+export const SELECT_TARGET = 'selectTarget';
 export const CHANGE_BATMAN_POSITION = 'changeBatmanPosition';
