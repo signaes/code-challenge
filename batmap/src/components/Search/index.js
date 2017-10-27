bcba3c1b15bc8ec272b9e7537bf22862d9732f80
Thiago Oliveira <thiago@signaes.co>
2017-10-27 01:51:15 -0200

    refactor: Use class with controlled inputs

--- batmap/src/components/Search/index.js
+++ batmap/src/components/Search/index.js
@@ -3,18 +3,68 @@ import React from 'react';
 import './styles.css';
 import { lat, lng } from '../../resources/gotham';
 
-const Search = () => (
-  <div className="Search">
-    <div className="Search__inputs-container">
-      <div className="Search__lat">
-        <input type="number" name="lat" min={lat.min} max={lat.max} placeholder="lat" />
+class Search extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.setSearchLat = this.setSearchLat.bind(this);
+    this.setSearchLng = this.setSearchLng.bind(this);
+    this.initializeMap = this.initializeMap.bind(this);
+  }
+
+  setSearchLat() {
+    this.props.setSearchLat(this.lat.value);
+  }
+
+  setSearchLng() {
+    this.props.setSearchLng(this.lng.value);
+  }
+
+  initializeMap() {
+    if (this.props.search.lat && this.props.search.lng) {
+      this.props.initializeMap({
+        container: this.props.map.container,
+        center: {
+          lat: this.props.search.lat,
+          lng: this.props.search.lng
+        }
+      });
+    }
+  }
+
+  render() {
+    return (
+      <div className="Search">
+        <div className="Search__inputs-container">
+          <div className="Search__lat">
+            <input
+              type="number"
+              name="lat"
+              min={lat.min}
+              max={lat.max}
+              ref={lat => this.lat = lat}
+              onChange={this.setSearchLat}
+              placeholder="lat" />
+          </div>
+          <div className="Search__lng">
+            <input
+              type="number"
+              name="lng"
+              min={lng.min}
+              max={lng.max}
+              ref={lng => this.lng = lng}
+              onChange={this.setSearchLng}
+              placeholder="lng" />
+          </div>
+        </div>
+        <button
+          className="Button Button--search"
+          onClick={this.initializeMap}>
+          Search this location
+        </button>
       </div>
-      <div className="Search__lng">
-        <input type="number" name="lng" min={lng.min} max={lng.max} placeholder="lng" />
-      </div>
-    </div>
-    <button className="Button Button--search">Search this location</button>
-  </div>
-);
+    );
+  }
+}
 
 export default Search;
