ff52e00e8406efb10e2844ac624057becf1c4a6b
Thiago Oliveira <thiago@signaes.co>
2017-10-27 02:33:22 -0200

    refactor: Open/close component

--- batmap/src/components/Villain/index.js
+++ batmap/src/components/Villain/index.js
@@ -6,38 +6,63 @@ import humanizeProbability from '../../utils/humanizeProbability';
 
 const isActiveTarget = (target, targets) => target.place === targets.active.place;
 
-const Villain = ({ villain, selectTarget }) => (
-  <aside className="Villain">
-    <header className="Villain__header">
-      <h2>{ villain.name } is around</h2>
-    </header>
-    <div className="Targets">
-      <h3 className="Targets__title">Possible targets</h3>
-      <ul className="Targets__list">
-        { villain.targets.list.map((t, i) => (
-          <li
-            className={`Target${ isActiveTarget(t, villain.targets) ? ' Target--active' : '' }`}
-            onClick={() => selectTarget({ target: t })}
-            key={i}>
-            <h4 className="Target__name">{ t.place }</h4>
-            <small className="Target__probability">
-              { humanizeProbability(t.probability) } probability ({ displayFloat(t.probability) })
-            </small>
-            <br />
-            <small className="Target__location">
-              { displayFloat(t.location.lat) }, { displayFloat(t.location.lng) }
-            </small>
-            <div className="Chart">
-              <div
-                className="Bar"
-                data-probability={humanizeProbability(t.probability)}
-                style={{width: `${parseInt(t.probability, 10)}%`}} />
-            </div>
-          </li>
-        )) }
-      </ul>
-    </div>
-  </aside>
-);
+class Villain extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      initialHeight: '436px',
+      height: '436px',
+      visible: true
+    };
+
+    this.toggleHeight = this.toggleHeight.bind(this);
+  }
+
+  toggleHeight() {
+    this.setState({
+      height: this.state.visible ? '80px' : this.state.initialHeight,
+      visible: !this.state.visible
+    });
+  }
+
+  render() {
+    const { villain, selectTarget } = this.props;
+
+    return (
+      <aside className="Villain" data-visible={this.state.visible} style={{ height: this.state.height }}>
+        <header className="Villain__header" onClick={this.toggleHeight}>
+          <h2>{ villain.name } is around</h2>
+        </header>
+        <div className="Targets">
+          <h3 className="Targets__title">Possible targets</h3>
+          <ul className="Targets__list">
+            { villain.targets.list.map((t, i) => (
+              <li
+                className={`Target${ isActiveTarget(t, villain.targets) ? ' Target--active' : '' }`}
+                onClick={() => selectTarget({ target: t })}
+                key={i}>
+                <h4 className="Target__name">{ t.place }</h4>
+                <small className="Target__probability">
+                  { humanizeProbability(t.probability) } probability ({ displayFloat(t.probability) })
+                </small>
+                <br />
+                <small className="Target__location">
+                  { displayFloat(t.location.lat) }, { displayFloat(t.location.lng) }
+                </small>
+                <div className="Chart">
+                  <div
+                    className="Bar"
+                    data-probability={humanizeProbability(t.probability)}
+                    style={{width: `${parseInt(t.probability, 10)}%`}} />
+                </div>
+              </li>
+            )) }
+          </ul>
+        </div>
+      </aside>
+    );
+  }
+}
 
 export default Villain;
