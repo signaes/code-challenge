294ba34dfa8be0a9fb3dee4d67c37190eda25b4c
Thiago Oliveira <thiago@signaes.co>
2017-10-27 01:03:10 -0200

    feat: Update map height calc

--- batmap/src/components/Map/index.js
+++ batmap/src/components/Map/index.js
@@ -3,23 +3,33 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import './styles.css';
 
+const setContainerHeight = container => {
+  container.style.height = `${document.body.offsetHeight}px`;
+};
+
 class Map extends Component {
   componentDidMount() {
     const { initializeMap, addMarker, findVillain } = this.props;
     const { currentPosition } = this.props.batman;
     const { lat, lng } = currentPosition;
 
-    this.container.style.height = `${document.body.offsetHeight}px`;
+    setContainerHeight(this.container);
+
+    window.addEventListener('resize', () => {
+      setContainerHeight(this.container);
+    });
 
     initializeMap({
       container: this.container,
       center: currentPosition
     });
+
     addMarker({
       lat,
       lng,
       title: 'Batman'
     });
+
     findVillain(currentPosition);
   }
 
